0d5ebfd9bb0d0bce9bb545144c5ccf4603c1738a
dungtng <86867357+dungtng@users.noreply.github.com>
2022-11-05 17:25:09 +0700

    Add files via upload

--- ex1.java
+++ ex1.java
@@ -8,13 +8,24 @@ public class ex1 {
 
 	public static void main(String[] args) {
 		int[] arr = { 50, 25, 5, 20, 10 };
+//		for (int i = 0; i < arr.length; i++) {
+//			if (i + 1 < arr.length) {
+//				if (arr[i] > arr[i + 1]) {
+//					int number = arr[i];
+//					arr[i] = arr[i + 1];
+//					arr[i + 1] = number;
+//				}
+//			}
+//		}
 		for (int i = 0; i < (arr.length); i++) {
-			for (int j = i + 1; j < arr.length; j++) {
-				if (arr[i] > arr[j]) {
-					int number = arr [i];
-					arr[i] = arr[j];
-					arr[j] = number;
-					
+			for (int j = 0; j < arr.length; j++) {
+				if (j + 1 < arr.length) {
+					if (arr[j] > arr[j+1]) {
+						int number = arr [j];
+						arr[j] = arr[j+1];
+						arr[j+1] = number;
+						
+					}
 				}
 			}
 		}
