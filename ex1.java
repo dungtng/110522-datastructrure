e9bd5eb5870ccb7c479f19f43091e9336fac48ea
dungtng <86867357+dungtng@users.noreply.github.com>
2022-11-05 17:15:31 +0700

    Update ex1.java

--- ex1.java
+++ ex1.java
@@ -8,15 +8,6 @@ public class ex1 {
 
 	public static void main(String[] args) {
 		int[] arr = { 50, 25, 5, 20, 10 };
-//		for (int i = 0; i < arr.length; i++) {
-//			if (i + 1 < arr.length) {
-//				if (arr[i] > arr[i + 1]) {
-//					int number = arr[i];
-//					arr[i] = arr[i + 1];
-//					arr[i + 1] = number;
-//				}
-//			}
-//		}
 		for (int i = 0; i < (arr.length); i++) {
 			for (int j = i + 1; j < arr.length; j++) {
 				if (arr[i] > arr[j]) {
